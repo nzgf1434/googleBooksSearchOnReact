a667024bfcb8d9145aa3eceb58ea793fda090bbb
denis.malyshev <denis.malyshev@artezio.ru>
2019-12-05 14:05:08 +0400

    fixed errors with history path

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,59 +1,73 @@
-import React, { Component } from 'react';
-import './app.css';
-import BooksList from '../books-list';
+import React, { Component } from "react";
+import "./app.css";
+import BooksList from "../books-list";
 import SearchPanel from "../search-panel";
-import GoogleBooksService from '../../services/GoogleBooksService';
-import DetailBooksItem from '../details-books-item';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-
+import GoogleBooksService from "../../services/GoogleBooksService";
+import DetailBooksItem from "../details-books-item";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
-
   state = {
-    searchText: '',
-    itemsBooks: null, 
+    searchText: "",
+    itemsBooks: null,
     details: null
-  }
-  
+  };
+
   booksService = new GoogleBooksService();
 
-  onSearchChange = (text) => {
-    this.setState({searchText: text});
-  }
+  onSearchChange = text => {
+    this.setState({ searchText: text });
+  };
 
-  onDetailsClick = (id) => {
-    const currentBook = this.state.itemsBooks.filter((item) => item.id === id);
-    this.setState({details: currentBook[0]});
-  }
+  onDetailsClick = id => {
+    const currentBook = this.state.itemsBooks.filter(item => item.id === id);
+    this.setState({ details: currentBook[0] });
+  };
 
   componentDidUpdate(prevProps, prevState) {
-    if (this.state.searchText && prevState.searchText !== this.state.searchText){
-      this.booksService.getSearchResult(this.state.searchText)
-      .then((res) => {
+    if (
+      this.state.searchText &&
+      prevState.searchText !== this.state.searchText
+    ) {
+      this.booksService.getSearchResult(this.state.searchText).then(res => {
         this.setState(() => {
-          return {itemsBooks: res.items}})
-      })
+          return { itemsBooks: res.items };
+        });
+      });
     }
   }
 
   render() {
-
     // if (this.state.searchText && !this.state.details){
-      if (this.state.searchText){
+    if (this.state.searchText) {
       return (
         <Router>
           <div className="App container">
-            <Route path="/" 
-                    render={() => <SearchPanel onSearchChange={this.onSearchChange}/>} 
-              />
-            <Route path="/list" 
-                  render={() => <BooksList list={this.state.itemsBooks} onDetailsClick={this.onDetailsClick} />} exact
+            <Route
+              path="/"
+              render={() => (
+                <SearchPanel onSearchChange={this.onSearchChange} />
+              )}
             />
-            <Route path="/list/:id" 
-                  render={() => <DetailBooksItem details={this.state.details} />}
-                    // const { id } = match.params; itemId={id} 
-                   
+            <Route
+              path="/list"
+              render={() => (
+                <BooksList
+                  list={this.state.itemsBooks}
+                  onDetailsClick={this.onDetailsClick}
+                />
+              )}
+              exact
+            />
+            <Route
+              path="/list/:id"
+              render={({ match }) => {
+                // console.log(match);
+                const { id } = match.params;
+                return (
+                  <DetailBooksItem details={this.state.details} itemId={id} />
+                );
+              }}
             />
           </div>
         </Router>
@@ -64,7 +78,7 @@ class App extends Component {
     //     <Router>
     //       <div className="App container">
     //         <SearchPanel onSearchChange={this.onSearchChange}/>
-            
+
     //       </div>
     //     </Router>
     //   );
@@ -72,11 +86,10 @@ class App extends Component {
     else {
       return (
         <div className="App container">
-          <SearchPanel onSearchChange={this.onSearchChange}/>
+          <SearchPanel onSearchChange={this.onSearchChange} />
         </div>
-      )
+      );
     }
-    
   }
 }
 
--- src/components/books-list/books-list.js
+++ src/components/books-list/books-list.js
@@ -1,43 +1,33 @@
-import React, { Component } from 'react';
-import './books-list.css';
-import BooksItem from '../books-item/';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import "./books-list.css";
+import BooksItem from "../books-item/";
 
+const BooksList = props => {
+  const { list, onDetailsClick } = props;
 
- const BooksList = (props) => {
-
-    const { list, onDetailsClick, history } = props;
-  
-    if (list) {
-       const elements = list.map((
-           {id, volumeInfo: {title, authors, publishedDate, imageLinks = {}}}) => {
-            return (
-                <li key={id} className="container">
-                    <BooksItem 
-                        title={title}
-                        author={authors}
-                        date={publishedDate}
-                        picture={imageLinks.smallThumbnail}
-                        onDetailsClick = {() => {
-                           history.push(`/list/${id}`);
-                           console.log('history.push: ', history);
-                            return onDetailsClick(id)
-                        }}
-                    />
-                </li>
-            )
-        });
+  if (list) {
+    const elements = list.map(
+      ({
+        id,
+        volumeInfo: { title, authors, publishedDate, imageLinks = {} }
+      }) => {
         return (
-            <ul>
-                { elements }
-            </ul>
-        )
-    }
-    else {
-        return (
-            <ul>
-            </ul>
-        )
-    }
+          <li key={id} className="container">
+            <BooksItem
+              itemId={id}
+              title={title}
+              author={authors}
+              date={publishedDate}
+              picture={imageLinks.smallThumbnail}
+              onDetailsClick={onDetailsClick}
+            />
+          </li>
+        );
+      }
+    );
+    return <ul>{elements}</ul>;
+  } else {
+    return <ul></ul>;
+  }
 };
-export default withRouter(BooksList);
+export default BooksList;
